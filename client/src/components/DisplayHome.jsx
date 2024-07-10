a423b578a4c71d6c637d488402b3c49cda1f6fa8
Fardin2112 <fardinalam2112@gmail.com>
2024-07-11 02:36:00 +0530

    done for display home left some more UI

---
+++ client/src/components/DisplayHome.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Navbar from "./Navbar";
+import AlbumItem from "./AlbumItem";
+import { albumsData } from "../assets/assets";
+import { songsData } from "../assets/assets";
+import SongsItems from "./SongsItems";
+
+const DisplayHome = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              image={item.image}
+              desc={item.desc}
+              name={item.name}
+              id={item.id}
+            />
+          ))}
+        </div>
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hit</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item,index)=>(<SongsItems key={index} name={item.name} image={item.image} desc={item.desc}/>))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DisplayHome;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="w-full flex justify-between items-center font-semibold">
+        <div className="flex items-center gap-2">
+          <img
+            className="w-8 bg-black rounded-2xl cursor-pointer p-2"
+            src={assets.arrow_left}
+            alt=""
+          />
+          <img
+            className="w-8 bg-black rounded-2xl cursor-pointer p-2"
+            src={assets.arrow_right}
+            alt=""
+          />
+        </div>
+        <div className="flex items-center gap-4">
+          <p className="bg-white text-black rounded-2xl text-[15px] px-4 py-1 hidden md:block cursor-pointer">
+            Explore Premium{" "}
+          </p>
+          <p className=" bg-black rounded-2xl text-[15px] px-3 py-1 cursor-pointer">
+            Install App{" "}
+          </p>
+          <p className="bg-purple-500 text-black rounded-full w-7 h-7 flex items-center justify-center">
+            D
+          </p>
+        </div>
+      </div>
+      <div className="mt-4 flex items-center gap-2"> 
+        <p className="bg-white text-black px-4  py-1 rounded-2xl flex items-center justify-center cursor-pointer">All</p>
+        <p className=" bg-black px-4 py-1 rounded-2xl flex items-center justify-center cursor-pointer">Music</p>
+        <p className=" bg-black px-4 py-1 rounded-2xl flex items-center justify-center cursor-pointer">Podcasts</p>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
