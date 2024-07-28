74701ebfaa8fad37628d534747b388e77ea87610
Fardin2112 <fardinalam2112@gmail.com>
2024-07-29 00:11:46 +0530

    Now song is Added from admin panel to server next have to work on list

--- Admin/src/pages/AddSong.jsx
+++ Admin/src/pages/AddSong.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import {assets} from "../assets/assets"
+import axios from 'axios'
+import { url } from '../App';
+import { toast } from 'react-toastify';
 
 const AddSong = () => {
 
@@ -11,41 +14,72 @@ const AddSong = () => {
   const [loading,setLoading] = useState(false);
   const [albumData,setAlbumData] = useState([]);
 
-  const onSubmitHandler = () =>{
+  const onSubmitHandler = async (e) =>{
     e.preventDefault();
+    setLoading(true);
+    try {
+
+      const formData = new FormData();
+
+      formData.append('name',name)
+      formData.append('desc',desc)
+      formData.append('image',image)
+      formData.append('audio',song)
+      formData.append('album',album)
+
+      const responce = await axios.post(`${url}/api/song/add`,formData)
+
+      if(responce.data.success) {
+        toast.success("Song Added");
+          setName("")
+          setDesc("")
+          setAlbum("none")
+          setImage(false)
+          setSong(false)
+      } else {
+        toast.error("Something went wrong")
+      }
+      
+    } catch (error) {
+      toast.error("Error occured")
+    }
+    setLoading(false)
   }
 
-  return (
+  return loading ? 
+  <div className='grid place-items-center min-h-[80vh]'>
+    <div className='w-16 h-16 place-self-center border-4 border-gray-400 border-t-green-800 rounded-full animate-spin'></div>
+  </div> : (
     <form onSubmit={onSubmitHandler} className='flex flex-col items-start gap-8 text-gray-600'>
       <div className='flex gap-8'>
         <div className='flex flex-col gap-4'>
           <p>Upload Song</p>
-          <input type="file" accept="audio/*" id="song" hidden />
+          <input onChange={(e)=>setSong(e.target.files[0])} type="file" accept="audio/*" id="song" hidden />
           <label htmlFor="song">
-            <img src={assets.upload_song} alt="" className='w-28 cursor-pointer'  />
+            <img src={ song ? assets.upload_added : assets.upload_song} alt="" className='w-28 cursor-pointer'  />
           </label>
         </div>
         <div className='flex flex-col gap-4'>
           <p>upload image</p>
-          <input type="file" id='image' accept='image/*' hidden />
+          <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='image' accept='image/*' hidden />
           <label htmlFor="image">
-            <img src={assets.upload_area} className='w-28 cursor-pointer' alt="" />
+            <img src={ image ? URL.createObjectURL(image) : assets.upload_area} className='w-28 cursor-pointer' alt="" />
           </label>
         </div>
       </div>
 
       <div className='flex flex-col gap-2.5'>
         <p>Song name</p>
-        <input type="text" className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vh,250px)]' placeholder='Type Here' required />
+        <input onChange={(e)=>setName(e.target.value)} value={name} type="text" className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vh,250px)]' placeholder='Type Here' required />
       </div>
       <div className='flex flex-col gap-2.5'>
         <p>Song description</p>
-        <input type="text" className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vh,250px)]' placeholder='Type Here' required />
+        <input onChange={(e)=>setDesc(e.target.value)} value={desc} type="text" className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vh,250px)]' placeholder='Type Here' required />
       </div>
 
       <div className='flex flex-col gap-2.5'>
         <p>Album</p>
-        <select className='bg-transparent outline-green-600 border-gray-400 border-2 p-2.5 w-[max(150px)]'>
+        <select onChange={(e)=>setAlbum(e.target.value)} defaultValue={album} className='bg-transparent outline-green-600 border-gray-400 border-2 p-2.5 w-[max(150px)]'>
           <option value="none">None</option>
         </select>
       </div>
