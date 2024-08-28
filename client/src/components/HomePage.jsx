d8f319dd937c577a05d33ed16a25e70a81f1535b
Fardin2112 <fardinalam2112@gmail.com>
2024-08-28 22:56:27 +0530

    Loading Spinner is added

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,29 +1,39 @@
 import { useContext } from "react";
-import Sidebar from "./Sidebar"
+import Sidebar from "./Sidebar";
 import Player from "./Player";
 //import Display from "./Display";
 import { PlayerContext } from "../context/PlayerContext";
 import DisplayHome from "./DisplayHome";
+import SyncLoader from "react-spinners/SyncLoader";
 
 const HomePage = () => {
-    const { audioRef, track, songsData } = useContext(PlayerContext);
-    return (
-      <div className="h-screen bg-black">
-        {songsData.length !== 0 ? (
-          <>
-            <div className="h-[90%] flex  ">
-              <Sidebar />
-              <DisplayHome />
-            </div>
-            <Player />
-          </>
-        ) : null}
-        <audio
-          ref={audioRef}
-          src={track ? track.file : ""}
-          preload="auto"
-        ></audio>
-      </div>
-    );
-} 
-export default HomePage
+  const { audioRef, track, songsData } = useContext(PlayerContext);
+  return (
+    <div className="h-screen bg-black">
+      {songsData.length !== 0 ? (
+        <>
+          <div className="h-[90%] flex  ">
+            <Sidebar />
+            <DisplayHome />
+          </div>
+          <Player />
+        </>
+      ) : (
+        <div className="flex justify-center items-center h-screen">
+          <SyncLoader
+            data-testid="loader"
+            aria-label="Loading Spinner"
+            color="#dfdfdf"
+            margin={2}
+          />
+        </div>
+      )}
+      <audio
+        ref={audioRef}
+        src={track ? track.file : ""}
+        preload="auto"
+      ></audio>
+    </div>
+  );
+};
+export default HomePage;
